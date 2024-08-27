283e99dbbc345807adb0ee300be1857bf383e82f
itachi-p <itachip38@gmail.com>
2024-08-27 14:03:59 +0900

    feat: Set login-page as homepage and add post-login screen

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="mt-5">
+        <h2 class="text-muted text-center">No posts yet.</h2>
+        <p class="text-center">
+            <a href="" class="text-decoration-none">Create a new post</a>
+        </p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+# Middleware ~~ will not any users that are not authenticated/logged in.
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+});
