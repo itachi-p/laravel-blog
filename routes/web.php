e9de9f9460d32eaed2d50fa90a3438df2f99231d
itachi-p <itachip38@gmail.com>
2024-08-31 09:40:59 +0900

    feat: Deletion confirmation modal (re)implemented

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,10 @@ use App\Http\Controllers\UserController;
 
 Auth::routes();
 
-# Middleware ~~ will not any users that are not authenticated/logged in.
+# Middleware ~~ will not allow any users that are not authenticated/logged in.
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [PostController::class, 'index'])->name('index');
 
-    // Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
 
     // POST - This Route Group will organize or group all routes related to POST
     Route::group(['prefix' => 'post', 'as' => 'post.'], function () {
